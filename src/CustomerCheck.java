3a3dc5c438a8f74699de6a216825f1b29703e7b8
unknown <BT_1E10_16@BKTECH.LOCAL>
2022-02-08 10:53:22 -0500

    Initial commit

---
+++ src/CustomerCheck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class CustomerCheck
+{
+    /** The check for a customer or group of customers
+     *  Guaranteed to contain at least one MenuItem and all entries are non-null
+     */
+    private ArrayList<MenuItem> check;
+
+    /* Constructor */
+    public CustomerCheck(ArrayList<MenuItem> check)
+    {
+        this.check = check;
+    }
+
+    /** Returns the total of all MenuItem prices on the check,
+     *  as described in part (a)
+     */
+    public double totalPrices()
+    {
+        int total = 0;
+        for (int i = 0; i < check.size(); i++)
+        {
+            total += check.get(i).getPrice();
+        }
+        return total;
+    }
+
+    /** Returns true if the restaurant’s coupon offer can be applied to this check and
+     *  returns false otherwise, as described in part (b) */
+    public boolean couponApplies()
+    {
+        if(totalPrices() < 40 || )
+        {
+            return false;
+        }
+        return true;
+    }
+
+    /** Calculates the final cost of this check, as described in part (c) */
+    public double calculateCheck()
+    {
+        /* to be implemented in part (c) */
+    }
+}
