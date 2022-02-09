606e1c8ea8522913fb8a77aa2b4db518ff505450
Luexi <lzhao9381@bths.edu>
2022-02-08 19:40:10 -0500

    Finalizing the lab with a, b, c

--- src/CustomerCheck.java
+++ src/CustomerCheck.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 
 public class CustomerCheck
@@ -18,7 +19,7 @@ public class CustomerCheck
      */
     public double totalPrices()
     {
-        int total = 0;
+        double total = 0;
         for (int i = 0; i < check.size(); i++)
         {
             total += check.get(i).getPrice();
@@ -30,9 +31,16 @@ public class CustomerCheck
      *  returns false otherwise, as described in part (b) */
     public boolean couponApplies()
     {
-        if(totalPrices() < 40 || )
+        if(totalPrices() < 40)
         {
             return false;
+        }
+        for(MenuItem item : check)
+        {
+            if(item.isDailySpecial())
+            {
+                return false;
+            }
         }
         return true;
     }
@@ -40,7 +48,29 @@ public class CustomerCheck
     /** Calculates the final cost of this check, as described in part (c) */
     public double calculateCheck()
     {
-        /* to be implemented in part (c) */
-        System.out.println("hello");
+        double originalCost = totalPrices();
+        double finalCost = originalCost;
+        int customers = 0;
+
+
+        for(MenuItem item : check)
+        {
+            if(item.isEntree())
+            {
+                customers++;
+            }
+        }
+        if(customers >= 6)
+        {
+            finalCost += originalCost * 0.2;  // 20 percent tax :(
+        }
+        if(couponApplies())
+        {
+            finalCost -= originalCost * 0.25;
+        }
+
+        return finalCost;
+
+
     }
 }
